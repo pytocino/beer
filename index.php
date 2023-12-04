d9209f435db3ffd3b4a868c4d7ef48a43fc74216
pygucci <pedrodavidg88@gmail.com>
2023-12-04 17:14:09 +0100

    paginacion1

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ $metaDescripcion = "Encuentra fácilmente bares y restaurantes que sirven la cer
         </div>
         <div class="row">
             <div class="col-12">
-                <form class="form-check" action="enviar_correo.php" method="post" enctype="text/plain">
+                <form class="form-check pl-0" action="enviar_correo.php" method="post" enctype="text/plain">
                     <h2>CONTACTO</h2>
                     <p>¿Tienes alguna pregunta o comentario? ¡Contáctanos!</p>
                     <div class="form-group mb-2">

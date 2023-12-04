2f6ad8e67e98e429f397541fef99f41a79e3c28f
pygucci <pedrodavidg88@gmail.com>
2023-12-04 22:58:17 +0100

    paginacion final

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ $metaDescripcion = "Encuentra fácilmente bares y restaurantes que sirven la cer
             </div>
         </div>
     </header>
-    <main class="container mt-4">
+    <main class="container mt-5">
         <div class="row">
             <div class="col-12">
                 <div class="background-image d-flex justify-content-center align-items-center">
